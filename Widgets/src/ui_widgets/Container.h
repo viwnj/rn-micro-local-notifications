631af28c371aadfa27d657b27ad8760c1e9e05ce
Vitor Henrique <vitorhprz@gmail.com>
2020-06-11 18:38:55 -0300

    Move virtual function definitions to header file

--- Widgets/src/ui_widgets/Container.h
+++ Widgets/src/ui_widgets/Container.h
@@ -8,8 +8,10 @@ protected:
 	std::vector<Container> children;
 public:
 	Container();
-	virtual void render();
-	virtual void update();
+	virtual void render(){};
+	virtual void update(){};
+	virtual SDL_Rect get_rect() { return SDL_Rect{0, 0, 0, 0}; };
+	virtual void onclick(){};
 	void append_child(Container container);
 	void remove_child();
 	void set_display_mode(std::string& display_mode);
