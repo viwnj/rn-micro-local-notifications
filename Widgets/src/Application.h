5e57fa0f3fa8a027d2faab233c73521694cb74fe
Vitor Henrique <vitorhprz@gmail.com>
2020-06-11 18:40:47 -0300

    add_widget function

--- Widgets/src/Application.h
+++ Widgets/src/Application.h
@@ -2,9 +2,13 @@
 
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <vector>
+#include <memory>
 #include "Renderer.h"
 #include "geom/Dimension.h"
 #include "geom/Position.h"
+#include "ui_widgets/Container.h"
+#include "Collision.h"
 
 class Application
 {
@@ -14,6 +18,7 @@ class Application
 		SDL_Window* window;
 		SDL_Event event;
 		SDL_Renderer* renderer;
+		std::vector<std::shared_ptr<Container>> clickable_elements;
 		bool should_quit;
 		int ticks_last_frame;
 		float get_delta_time();
@@ -27,4 +32,12 @@ public:
 		void process_input();
 		void render();
 		void on_mouse_press(SDL_MouseButtonEvent& button);
+		template <typename T, typename ...TArgs>
+		void add_widget(TArgs ...args) {
+			std::shared_ptr<Container> widget = std::make_shared<T>(std::forward<TArgs>(args)...);
+
+			if (widget->is_interactive) {
+				clickable_elements.emplace_back(widget);
+			}
+		}
 };
